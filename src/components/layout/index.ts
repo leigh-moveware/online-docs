090963c1ec8427b07d584ff633d36646aff95bdf
Net Ninjas <leigh.morrow@moveconnect.com>
2026-02-04 11:26:23 +1100

    Clean branch

--- src/components/layout/index.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Export barrel for layout components
- */
-
-export { PageShell, PageHeader } from './PageShell';
-export type { PageShellProps, PageHeaderProps } from './PageShell';
--- src/theme/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Theme configuration export
- */
-
-export { colors } from './colors';
-export type { ColorToken } from './colors';
-
-export { typography } from './typography';
-export type { Typography } from './typography';
